1a3679ccf7de4bd978d49ce5b0e530742a4d9ed3
Rhowen01 <rraju002@ucr.edu>
2022-08-18 20:07:11 -0700

    complete update

--- index.js
+++ index.js
@@ -1,75 +1,87 @@
-// TODO: Include packages needed for this application
-const inquirer = require("inquirer")
-const fs = require("fs") 
-// TODO: Create an array of questions for user input
-const questions = [
-    {
-        type: "input", 
-        message: "What is the Title of Your README?", 
-        name: "title"
-    },
+// Declaring the dependencies and variables
+const fs = require("fs");
+const util = require("util");
+const inquirer = require("inquirer");
+const generateReadme = require("./utils/generateReadme")
+const writeFileAsync = util.promisify(fs.writeFile);
 
-    {
-        type: "input", 
-        message: "Describe your README!", 
-        name: "description"
-    },
-    
-    {
-        type: "input", 
-        message: "Provide Installation Instructions!", 
-        name: "install"
-    }, 
+//Prompt the user questions to populate the README.md
+function promptUser(){
+    return inquirer.prompt([
+        {
+            type: "input",
+            name: "projectTitle",
+            message: "What is the project title?",
+        },
+        {
+            type: "input",
+            name: "description",
+            message: "Write a brief description of your project: "
+        },
+        {
+            type: "input",
+            name: "installation",
+            message: "Describe the installation process if any: ",
+        },
+        {
+            type: "input",
+            name: "usage",
+            message: "What is this project usage for?"
+        },
+        {
+            type: "list",
+            name: "license",
+            message: "Chose the appropriate license for this project: ",
+            choices: [
+                "Apache",
+                "Academic",
+                "GNU",
+                "ISC",
+                "MIT",
+                "Mozilla",
+                "Open"
+            ]
+        },
+        {
+            type: "input",
+            name: "contributing",
+            message: "Who are the contributors of this projects?"
+        },
+        {
+            type: "input",
+            name: "tests",
+            message: "Is there a test included?"
+        },
+        {
+            type: "input",
+            name: "questions",
+            message: "What do I do if I have an issue? "
+        },
+        {
+            type: "input",
+            name: "username",
+            message: "Please enter your GitHub username: "
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "Please enter your email: "
+        }
+    ]);
+} 
 
-    {
-        type: "input", 
-        message: "What is the Project's Usage For?", 
-        name: "usage"
-    },
-
-    {
-        type: "list", 
-        message: "What License Will you be Using for your Project?", 
-        choices: ['MIT License', 'GNU GPL v2', 'GNU GPL v3', 'Apache License 2.0', 'BSD 3-Clause'],
-        name: "license"
-    },  
-
-    {
-        type: "input", 
-        message: "What are the Contribution Guidelines for This Project?", 
-        name: "contribution"
-    }, 
-
-    {
-        type: "input", 
-        message: "What are the Instructions for this Test?", 
-        name: "test"
-    },
-
-    {
-        type: "input", 
-        message: "What do I do if I Have an Issue?", 
-        name: "questions"
-    }, 
-
-    {
-        type: "input", 
-        message: "Enter a Username!", 
-        name: "username"
-    }, 
-
-    {
-        type: "input", 
-        message: "What is Your Email?", 
-        name: "email"
-    }, 
-];
-
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
-
-// TODO: Create a function to initialize app
-function init() {}
-
-// Function call to initialize app
-init();
+// Async function using util.promisify 
+  async function init() {
+    try {
+        // Ask user questions and generate responses
+        const answers = await promptUser();
+        const generateContent = generateReadme(answers);
+        // Write new README.md to dist directory
+        await writeFileAsync('./dist/README.md', generateContent);
+        console.log('✔️  Successfully wrote to README.md');
+    }   catch(err) {
+        console.log(err);
+    }
+  }
+  
+  init();  
